1adbd2e483f2a03ec153fc9c1ec70cfeacf7cd17
MelounekBro <melounscrappyart@gmail.com>
2026-04-15 23:55:49 +0200

    fixed frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/posts');
@@ -8,6 +9,8 @@ const likeRoutes = require('./routes/likes');
 const userRoutes = require('./routes/users');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
